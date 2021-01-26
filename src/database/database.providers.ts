394e477bb300a165deea975c44857cff97556afd
Tom Nagle <tomanagle@gmail.com>
2021-01-27 00:03:12 +1000

    feat: constants

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,10 +1,11 @@
 // src/database/database.providers.ts
 import mongoose from 'mongoose';
+import { MONO_DB_CONNECTION_STRING } from '../constants';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/nest'),
+      mongoose.connect(MONO_DB_CONNECTION_STRING),
   },
 ];
