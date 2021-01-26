92d7c46b9ff36ebb2aca730effe6522c2aad92e2
Tom Nagle <tomanagle@gmail.com>
2021-01-26 22:56:38 +1000

    first commit

---
+++ src/database/database.providers.ts
@@ -0,0 +1,10 @@
+// src/database/database.providers.ts
+import mongoose from 'mongoose';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: (): Promise<typeof mongoose> =>
+      mongoose.connect('mongodb://localhost/nest'),
+  },
+];
