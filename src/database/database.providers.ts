947aa2a59282ea97506c6bcb98dca714c04f038e
Ron Almog <ron8132@gmail.com>
2021-06-07 08:29:40 -0400

    typo, fix warnings on console

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,11 +1,14 @@
 // src/database/database.providers.ts
 import mongoose from 'mongoose';
-import { MONO_DB_CONNECTION_STRING } from '../constants';
+import { MONGO_DB_CONNECTION_STRING } from '../constants';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect(MONO_DB_CONNECTION_STRING),
+      mongoose.connect(MONGO_DB_CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
   },
 ];
